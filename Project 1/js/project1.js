657810b518834e92c69117a3ab2f586dcc64e14b
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-02-26 14:18:42 -0500

    Step 5

--- Project 1/js/project1.js
+++ Project 1/js/project1.js
@@ -14,4 +14,53 @@ $(function () {
 
     $('figcaption').html("<em>"+alt+"</em><br>"+title);
     });
+    //Step 5: set up event listeners for the input event of each of the range sliders.(Ammu)
+
+    $('#sliderOpacity').on('input', function() {
+        $('#numOpacity').html($(this).val());
+        $(this).attr('value',$(this).val());
+        imageManipulation();
+    });
+
+    $('#sliderSaturation').on('input', function() {
+        $('#numSaturation').html($(this).val());
+        $(this).attr('value',$(this).val());
+        imageManipulation();
+    });
+
+    $('#sliderBrightness').on('input',function() {
+        $('#numBrightness').html($(this).val());
+        $(this).attr('value',$(this).val());
+        imageManipulation();
+    });
+
+    $('#sliderHue').on('input',function() {
+        $('#numHue').html($(this).val());
+        $(this).attr('value',$(this).val());
+        imageManipulation();
+    });
+
+    $('#sliderGray').on('input',function() {
+        $('#numGray').html($(this).val());
+        $(this).attr('value',$(this).val());
+        imageManipulation();
+    });
+
+    $('#sliderBlur').on('input',function() {
+        $('#numBlur').html($(this).val());
+        $(this).attr('value',$(this).val());
+        imageManipulation();
+    });
+
+    //Step 5: To set filter and -web-kit-filter properties on the image in the <figure> (Ammu)
+    function imageManipulation(){
+        var $op=$('#sliderOpacity').attr('value');
+        var $sa=$('#sliderSaturation').attr('value');
+        var $br=$('#sliderBrightness').attr('value');
+        var $hu=$('#sliderHue').attr('value');
+        var $gr=$('#sliderGray').attr('value');
+        var $bl=$('#sliderBlur').attr('value');
+        $('#imgManipulated img').css('filter', 'opacity('+$op+'%) saturate('+$sa+'%) brightness('+$br+'%) hue-rotate('+$hu+'deg) grayscale('+$gr+'%) blur('+$bl+'px)');
+        $('#imgManipulated img').css('-webkit-filter', 'opacity('+$op+'%) saturate('+$sa+'%) brightness('+$br+'%) hue-rotate('+$hu+'deg) grayscale('+$gr+'%) blur('+$bl+'px)');
+    }
 });
