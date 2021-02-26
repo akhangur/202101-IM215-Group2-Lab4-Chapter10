e560fd2f9b33770f267b9b4bc78ce1a50bb5af8f
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-02-26 14:16:54 -0500

    Step 3: addlistener and replace the folder name

--- Project 1/js/project1.js
+++ Project 1/js/project1.js
@@ -1,1 +1,14 @@
+$(function () {
+    //Step 3: Add listener to click event on the painting thumbnails. (Bony)
 
+    $('div img').on('click', function (e) {
+    var alt = $(this).attr('alt');
+    var title=$(this).attr('title');
+    var src = $(this).attr('src');
+
+    //Step 3: Replace the folder name small with medium in src attribute. (Bony)
+
+    var newsrc = src.replace("small","medium");
+    $('figure img').attr("src",newsrc);
+    });
+});
