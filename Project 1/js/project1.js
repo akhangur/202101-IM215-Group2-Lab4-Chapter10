4946a63f3041fcba93e77cb6968d05dc9ded16d6
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-02-26 14:19:27 -0500

    Step 6

--- Project 1/js/project1.js
+++ Project 1/js/project1.js
@@ -14,6 +14,7 @@ $(function () {
 
     $('figcaption').html("<em>"+alt+"</em><br>"+title);
     });
+
     //Step 5: set up event listeners for the input event of each of the range sliders.(Ammu)
 
     $('#sliderOpacity').on('input', function() {
@@ -63,4 +64,26 @@ $(function () {
         $('#imgManipulated img').css('filter', 'opacity('+$op+'%) saturate('+$sa+'%) brightness('+$br+'%) hue-rotate('+$hu+'deg) grayscale('+$gr+'%) blur('+$bl+'px)');
         $('#imgManipulated img').css('-webkit-filter', 'opacity('+$op+'%) saturate('+$sa+'%) brightness('+$br+'%) hue-rotate('+$hu+'deg) grayscale('+$gr+'%) blur('+$bl+'px)');
     }
+    //Step 6: To add listener for the click event of the reset button (Ammu)
+    $('#resetFilters').on('click',function(e) {
+        e.preventDefault();
+        //Step 6: To remove filters from the image (Amandeep)
+        $('#imgManipulated img').css('filter','');
+        $('#imgManipulated img').css('-webkit-filter','');  
+        //Step 6:Reset the value of attribute (Ammu)
+        $('form')[0].reset();
+        $('#sliderOpacity').attr('value','100');  
+        $('#sliderSaturation').attr('value','100');  
+        $('#sliderBrightness').attr('value','100');
+        $('#sliderHue').attr('value','0');
+        $('#sliderGray').attr('value','0');
+        $('#sliderBlur').attr('value','0');
+        $('#numOpacity').html($('#sliderOpacity').attr('value'));
+        $('#numSaturation').html($('#sliderSaturation').attr('value'));
+        $('#numBrightness').html($('#sliderBrightness').attr('value'));
+        $('#numHue').html($('#sliderHue').attr('value'));
+        $('#numGray').html($('#sliderGray').attr('value'));
+        $('#numBlur').html($('#sliderBlur').attr('value'));
+    })
+
 });
